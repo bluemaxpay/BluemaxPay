76276688e44a05b11c9813fe891cd2f9baa6d5ec
Zillotech <sales@zillotech.com>
2024-02-24 10:51:26 -0600

    Remove donations and forum

--- bluemax_forum/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import forum_discussion
-from . import forum_discussion_post
-from . import rank_type
-from . import res_users
-from . import website
-__all__ = ['forum_discussion', 'forum_discussion_post', 'website', 'res_users']
