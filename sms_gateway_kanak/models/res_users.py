55d2f899b2643db7d97883563ad1bc439426b305
Zillotech <sales@zillotech.com>
2024-03-08 17:56:34 -0600

    remove sms app

--- sms_gateway_kanak/models/res_users.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    mobile = fields.Char(related='partner_id.mobile', string="Mobile No")
--- sms_gateway_kanak/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import send_custom_sms_wizard
-from . import sms_gateway_composer
-from . import sms_marketing_schedule_date
