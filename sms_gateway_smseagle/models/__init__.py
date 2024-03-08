752fe39f2e3cbb4df200fbcd92d5283498315da8
Zillotech <sales@zillotech.com>
2024-03-08 17:57:45 -0600

    update sms

---
+++ sms_gateway_smseagle/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Powered by Kanak Infosystems LLP.
+# © 2020 Kanak Infosystems LLP. (<https://www.kanakinfosystems.com>).
+
+from . import discuss_channel
+from . import res_partner
+from . import sms_gateway
